4df25738bb4df14e1a077b927dd9e6a90d70e162
Jakub Pawlowicz <contact@jakubpawlowicz.com>
2011-04-17 12:10:57 +0200

    Added more binary tests.

--- test/binary-test.js
+++ test/binary-test.js
@@ -10,6 +10,10 @@ var withOptions = function(options) {
   };
 };
 
+var fullPath = function(suffix) {
+  return path.join(process.cwd(), suffix);
+};
+
 exports.commandsSuite = vows.describe('binary commands').addBatch({
   'no options': {
     topic: withOptions(),
@@ -78,6 +82,72 @@ exports.commandsSuite = vows.describe('binary commands').addBatch({
     'should give proper version': function(error, stdout) {
       var version = JSON.parse(fs.readFileSync('./package.json')).version;
       assert.include(stdout, version);
+    }
+  }
+});
+
+exports.packagingSuite = vows.describe('packaging all').addBatch({
+  'packaging without gzipped version': {
+    topic: withOptions('-r data/test1/public -c data/test1/assets.yml'),
+    'should not give error': function(error, stdout) {
+      assert.isNull(error);
+    },
+    'should compile css to less': function() {
+      assert.isTrue(path.existsSync(fullPath('test/data/test1/public/stylesheets/one.css')));
+      assert.isTrue(path.existsSync(fullPath('test/data/test1/public/stylesheets/two.css')));
+    },
+    'should bundle css into packages': function() {
+      assert.isTrue(path.existsSync(fullPath('test/data/test1/public/stylesheets/bundled/subset.css')));
+      assert.isTrue(path.existsSync(fullPath('test/data/test1/public/stylesheets/bundled/all.css')));
+    },
+    'should bundle css into compressed packages': function() {
+      assert.isFalse(path.existsSync(fullPath('test/data/test1/public/stylesheets/bundled/subset.css.gz')));
+      assert.isFalse(path.existsSync(fullPath('test/data/test1/public/stylesheets/bundled/all.css.gz')));
+    },
+    'should bundle js into packages': function() {
+      assert.isTrue(path.existsSync(fullPath('test/data/test1/public/javascripts/bundled/subset.js')));
+      assert.isTrue(path.existsSync(fullPath('test/data/test1/public/javascripts/bundled/all.js')));
+    },
+    'should bundle js into compressed packages': function() {
+      assert.isFalse(path.existsSync(fullPath('test/data/test1/public/javascripts/bundled/subset.js.gz')));
+      assert.isFalse(path.existsSync(fullPath('test/data/test1/public/javascripts/bundled/all.js.gz')));
+    },
+    teardown: function() {
+      exec('rm -rf ' + fullPath('test/data/test1/public/javascripts/bundled'));
+      exec('rm -rf ' + fullPath('test/data/test1/public/stylesheets/bundled'));
+      exec('rm -rf ' + fullPath('test/data/test1/public/stylesheets/*.css'));
+    }
+  }
+}).addBatch({
+  'packaging with gzipped version': {
+    topic: withOptions('-r data/test1/public -c data/test1/assets.yml -g'),
+    'should not give error': function(error, stdout) {
+      assert.isNull(error);
+    },
+    'should compile css to less': function() {
+      assert.isTrue(path.existsSync(fullPath('test/data/test1/public/stylesheets/one.css')));
+      assert.isTrue(path.existsSync(fullPath('test/data/test1/public/stylesheets/two.css')));
+    },
+    'should bundle css into packages': function() {
+      assert.isTrue(path.existsSync(fullPath('test/data/test1/public/stylesheets/bundled/subset.css')));
+      assert.isTrue(path.existsSync(fullPath('test/data/test1/public/stylesheets/bundled/all.css')));
+    },
+    'should bundle css into compressed packages': function() {
+      assert.isTrue(path.existsSync(fullPath('test/data/test1/public/stylesheets/bundled/subset.css.gz')));
+      assert.isTrue(path.existsSync(fullPath('test/data/test1/public/stylesheets/bundled/all.css.gz')));
+    },
+    'should bundle js into packages': function() {
+      assert.isTrue(path.existsSync(fullPath('test/data/test1/public/javascripts/bundled/subset.js')));
+      assert.isTrue(path.existsSync(fullPath('test/data/test1/public/javascripts/bundled/all.js')));
+    },
+    'should bundle js into compressed packages': function() {
+      assert.isTrue(path.existsSync(fullPath('test/data/test1/public/javascripts/bundled/subset.js.gz')));
+      assert.isTrue(path.existsSync(fullPath('test/data/test1/public/javascripts/bundled/all.js.gz')));
+    },
+    teardown: function() {
+      exec('rm -rf ' + fullPath('test/data/test1/public/javascripts/bundled'));
+      exec('rm -rf ' + fullPath('test/data/test1/public/stylesheets/bundled'));
+      exec('rm -rf ' + fullPath('test/data/test1/public/stylesheets/*.css'));
     }
   }
 });
